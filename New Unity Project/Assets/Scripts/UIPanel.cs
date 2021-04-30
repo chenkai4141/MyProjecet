9b7466718b30d12111a3610cce86b8336cff9d74
ck <393074421@qq.com>
2021-04-30 13:36:42 +0800

    4.30提交

---
+++ New Unity Project/Assets/Scripts/UIPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FairyGUI;
+using UnityEngine;
+
+public class UIPanel : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GRoot.inst.SetContentScaleFactor(800,600);
+        UIPackage.AddPackage("FGUI/MyTest");
+        Debug.Log("加载成功");
+        GComponent component = UIPackage.CreateObject("MyTest", "Demo_Main").asCom;
+        GRoot.inst.AddChild(component);
+    }
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
